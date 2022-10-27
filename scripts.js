dd7548364be377992ecb7cb1f4771a730e6e2f4f
Kareem Ellis <kareemellis55@gmail.com>
2022-10-27 14:37:14 -0500

    Referenced form objects

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 /* Add your JavaScript to this file */
+window.addEventListener('load', function () {
+    const email = document.querySelector("#email")
+    const submit = document.querySelector("button")
+    const message = document.querySelector(".message")
+})
