4d4cc44b81b11e70219432db8058c12cb3944594
Kareem Ellis <kareemellis55@gmail.com>
2022-10-27 20:32:29 -0500

    Moved valid() and invalid() out of submit Event Listener

--- scripts.js
+++ scripts.js
@@ -4,34 +4,35 @@ window.addEventListener('load', function () {
     const submit = document.querySelector("button")
     const message = document.querySelector(".message")
 
+    const valid = () => {
+        if(message.classList.contains("email-invalid")){
+            message.classList.remove("email-invalid")
+            message.classList.add("email-valid")
+        }
+        else{
+            message.classList.add("email-valid")
+        }
+
+        message.textContent = `Thank you! Your email address ${address} has been added to our mailing list!`
+    }
+
+    const invalid = () => {
+        if(message.classList.contains("email-valid")){
+            message.classList.remove("email-valid")
+            message.classList.add("email-invalid")
+        }
+        else{
+            message.classList.add("email-invalid")
+        }
+
+        message.textContent = "Please enter a valid email address"
+    }
+
     submit.addEventListener("click", (event) => {
         event.preventDefault()
 
         var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
         address = email.value
-        
-        const valid = () => {
-            if(message.classList.contains("email-invalid")){
-                message.classList.remove("email-invalid")
-                message.classList.add("email-valid")
-            }
-            else{
-                message.classList.add("email-valid")
-            }
-
-            message.textContent = `Thank you! Your email address ${address} has been added to our mailing list!`
-        }
-        const invalid = () => {
-            if(message.classList.contains("email-valid")){
-                message.classList.remove("email-valid")
-                message.classList.add("email-invalid")
-            }
-            else{
-                message.classList.add("email-invalid")
-            }
-
-            message.textContent = "Please enter a valid email address"
-        }
 
         if(address.match(pattern)){
             valid()
